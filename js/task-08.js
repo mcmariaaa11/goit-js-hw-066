e475d615aca8fb3867117e3cfcac6e6fb5ade2cf
mcmariaaa11 <mariamanea1108@gmail.com>
2024-02-27 18:37:26 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener  ("submit", e => {
+
+    e.preventDefault();
+  
+
+    const emailInput = loginForm.elements['email'];
+    const passwordInput = loginForm.elements['password'];
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+        alert('Te rugam sa introduci o valoare in ambele campuri!');
+        return;
+    
+};
+
+const formData = {
+    email: emailInput.value,
+    password: passwordInput.value
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
