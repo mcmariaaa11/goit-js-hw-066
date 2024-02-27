d76af91749121716e86c011a9ea2bb87a0aae4cd
mcmariaaa11 <mariamanea1108@gmail.com>
2024-02-27 19:12:08 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', function() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
